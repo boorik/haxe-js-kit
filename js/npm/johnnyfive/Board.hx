0732d8b9eb378ad01dad9ae8378f447ec378e4fe
boorik <vincent.blanchet.info@gmail.com>
2016-01-31 17:49:44 +0100

    add board properties

--- js/npm/johnnyfive/Board.hx
+++ js/npm/johnnyfive/Board.hx
@@ -3,6 +3,10 @@ package js.npm.johnnyfive;
 @native("js.npm.johnny-five.Board")
 extern class Board
 implements npm.Package.RequireNamespace<"johnny-five","*">{
+    public var id:Dynamic;
+    public var port:Dynamic;
+    public var debug:Bool;
+    public var repl:Dynamic;
     public function new():Void;
     function on(event:Dynamic,cb:Dynamic):Void;
 }
